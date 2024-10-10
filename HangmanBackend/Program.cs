aebdcb14fed555158419375f49d398a46899a5ea
André Siqueira <andre.siqueira@itlab.com.br>
2024-10-10 11:31:16 -0300

    removed comments

--- HangmanBackend/Program.cs
+++ HangmanBackend/Program.cs
@@ -2,22 +2,17 @@ using HangmanBackend.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adicionar o serviço de CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAllOrigins", builder =>
     {
-        builder.AllowAnyOrigin()  // Permite qualquer origem (atenção ao ambiente de produção)
-               .AllowAnyMethod()  // Permite qualquer método HTTP (GET, POST, etc.)
-               .AllowAnyHeader(); // Permite qualquer cabeçalho
+        builder.AllowAnyOrigin()  
+               .AllowAnyMethod()  
+               .AllowAnyHeader(); 
     });
 });
 
-// Registrar o GameSessionService como um serviço Singleton ou Transient
-builder.Services.AddSingleton<GameSessionService>(); // Singleton - uma única instância usada durante toda a aplicação
-                                                     // ou, se preferir, usar:
-                                                     // builder.Services.AddTransient<GameSessionService>(); // Transient - nova instância criada a cada solicitação
-
+builder.Services.AddSingleton<GameSessionService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -30,7 +25,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// Ativar CORS
 app.UseCors("AllowAllOrigins");
 
 app.UseAuthorization();
